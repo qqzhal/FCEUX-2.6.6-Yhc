b2458635b99cf5def0a19a80e4114c84027a9794
庄生 <qqzhal@163.com>
2026-09-08 00:55:37 +0800

    diagnostics: first-visit bitmap for block tracer (no silent cap)

--- src/boards/mmc3.cpp
+++ src/boards/mmc3.cpp
@@ -1163,6 +1163,7 @@ static void M195_HBClock(void) {
 static void M195_MapHook(int a) {
 	static uint32 ic = 0;
 	static uint16 lastBlk = 0xFFFF;
+	static uint8 seen[8192];	/* first-visit bitmap for 256-byte blocks */
 	/* scanline detector: clock the VirtuaNES-style IRQ once per scanline */
 	if (scanline != M195_lastScan) {
 		M195_lastScan = scanline;
@@ -1172,7 +1173,11 @@ static void M195_MapHook(int a) {
 	uint16 blk = X.PC & 0xFF00;
 	if (blk != lastBlk) {
 		lastBlk = blk;
-		if (M195_dbgn < 70000) M195Log("K PC=%04X A=%02X\n", X.PC, X.A);
+		uint32 bi = blk >> 3;
+		if (!(seen[bi >> 3] & (0x80 >> (bi & 7)))) {
+			seen[bi >> 3] |= (0x80 >> (bi & 7));
+			M195Log("K PC=%04X A=%02X\n", X.PC, X.A);
+		}
 	}
 	if (ic == 1000000 || ic == 5000000 || ic == 15000000)
 		M195Log("I %u PC=%04X en=%d cnt=%d latch=%d\n", ic, X.PC,
